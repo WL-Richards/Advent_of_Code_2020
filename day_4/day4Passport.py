d6ee2b83a680de4559cf8610f5d26459fb4c8982
Will Richards <jwilt9000@gmail.com>
2020-12-03 23:21:17 -0800

    Commented it to make it some what readable

--- day_4/day4Passport.py
+++ day_4/day4Passport.py
@@ -3,9 +3,16 @@ import copy
 
 
 def part_1(lines):
+    # Fields to check for existance
     check_fields = ["byr:", "iyr:", "eyr:", "hgt:", "hcl:", "ecl:", "pid:"]
+
+    # List of passport data, one entry per passport
     passports = []
+
+    # Number of passports counted
     num_passports = 0
+
+    # Stores tuples of the start and end indexes of the passport data
     passport_start_ends = []
 
     # Number of valid passports
@@ -13,24 +20,38 @@ def part_1(lines):
 
     # Parse the input to split able sections
     for i, line in enumerate(lines):
+
+        # Remove newlines and other useless data
         lines[i] = line.strip()
+
+        # If the length of the line is 1 (Which strangely seems to be one) it means there is nothing there
         if len(line) == 1:
+
+            # If it is the first passport being "scanned"
             if num_passports == 0:
+
+                # Start at index 0 for the first value
                 passport_start_ends.append((0, i))
                 num_passports += 1
+
+            # For all other passports with the exception of the last one uses one plus the previous passports endpoint
             else:
                 passport_start_ends.append((passport_start_ends[num_passports - 1][1] + 1, i))
                 num_passports += 1
+
+        # If it is the last passport set the end point to the last line in the file
         elif i == len(lines) - 1:
             passport_start_ends.append((passport_start_ends[num_passports - 1][1] + 1, len(lines)))
             num_passports += 1
 
+    # Use the passport indexes to take the data within and convert it to one line no matter its current form
     for i, data in enumerate(passport_start_ends):
         passport = ""
         for data in lines[passport_start_ends[i][0]:passport_start_ends[i][1]]:
             passport += str(data) + " "
         passports.append(passport)
 
+    # Loop through all passports and check if they have the required fields
     for passport in passports:
         valid = True
         for field in check_fields:
@@ -45,6 +66,8 @@ def part_1(lines):
 
 
 def validate_passport(passport):
+    """Used for part two to verify that passport data is valid"""
+
     # Seemingly valid passports that need to be verified
     split_passport_data = passport.split(" ")
     # Remove the last empty index
@@ -62,10 +85,12 @@ def validate_passport(passport):
         # Validate Birth Year
         if key_set[0] == "byr":
 
+            # Check if the string is actually a number first
             if not key_set[1].isnumeric():
                 passport_valid = False
                 break
 
+            # Make sure it is the correct length and it is within the given dates
             if len(key_set[1]) == 4 and 1920 <= int(key_set[1]) <= 2002:
                 passport_valid = True
             else:
@@ -74,27 +99,35 @@ def validate_passport(passport):
         # Validate Issue Year
         elif key_set[0] == "iyr":
 
+            # Check if the string is actually a number first
             if not key_set[1].isnumeric():
                 passport_valid = False
                 break
 
+            # Verify length and date range
             if len(key_set[1]) == 4 and 2010 <= int(key_set[1]) <= 2020:
                 passport_valid = True
             else:
                 passport_valid = False
                 break
+
         # Validate Expiration Year
         elif key_set[0] == "eyr":
+
+            # Verify length and date range
             if len(key_set[1]) == 4 and 2020 <= int(key_set[1]) <= 2030:
                 passport_valid = True
             else:
                 passport_valid = False
                 break
+
+        # Verify Height
         elif key_set[0] == "hgt":
 
             # Height without unit
             height = key_set[1][:-2]
 
+            # Check if the string is actually a number first
             if not height.isnumeric():
                 passport_valid = False
                 break
@@ -106,6 +139,7 @@ def validate_passport(passport):
                 else:
                     passport_valid = False
                     break
+
             # If height is inches
             else:
                 if 59 <= int(height) <= 76:
@@ -113,9 +147,17 @@ def validate_passport(passport):
                 else:
                     passport_valid = False
                     break
+
+        # Verify Hair Color
         elif key_set[0] == "hcl":
+
+            # Verify the total length is 7
             if len(key_set[1]) == 7:
+
+                # Then check to see if the first character is # signifying a hexcode
                 if key_set[1][0] == "#":
+
+                    # Then use regex to determine the alfa-numeric sequence following
                     if re.search("[a-z0-9]{6}", key_set[1][1:]):
                         passport_valid = True
                     else:
@@ -127,14 +169,22 @@ def validate_passport(passport):
             else:
                 passport_valid = False
                 break
+
+        # Verify Eye Color
         elif key_set[0] == "ecl":
+
+            # Just check if the eye color is equal to any of those
             if key_set[1] == "amb" or key_set[1] == "blu" or key_set[1] == "brn" or key_set[1] == "gry" or key_set[
                 1] == "grn" or key_set[1] == "hzl" or key_set[1] == "oth":
                 passport_valid = True
             else:
                 passport_valid = False
                 break
+
+        # Verify passport ID
         elif key_set[0] == "pid":
+
+            # Length is 9 and all numbers are numeric (regex is overkill, but I really don't care)
             if len(key_set[1]) == 9:
                 if re.search("[0-9]{9}", key_set[1]):
                     passport_valid = True
@@ -194,6 +244,8 @@ def part_2(lines):
 if __name__ == '__main__':
     # Open and read the lines out of the input file
     input_file = open("input.txt")
+
+    # Separate inputs because I am lazy and just edit the parameter itself, shallow copied into part 2
     part_1_input = input_file.readlines()
     part_2_input = copy.copy(part_1_input)
 
